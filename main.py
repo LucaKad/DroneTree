c8959d384a262cc7d8defcb9b1b1f62f54551a9c
luca <lucakad@knu.ua>
2024-03-03 12:22:12 +0200

    remove child limit

--- main.py
+++ main.py
@@ -1,123 +1,104 @@
 class TreeEvent:
-    def __init__(self, info, spread_up = False, spread_down = False):
+    def __init__(self, info, spread_up=False, spread_down=False):
         self.info = info
         self.spread_up = spread_up
         self.spread_down = spread_down
-class Node:
-    def __init__(self, key, parent):
-        self.key = key
-        self.left = None
-        self.right = None
-        self.parent = parent
 
-    #receive information from the commanding node or child node and spread if necessary
+
+class Node:
+    def __init__(self, key, parent, weight):
+        self.key = key
+        self.children = []
+        self.parent = parent
+        self.weight = weight
+
+    # receive information from the commanding node or child node and spread if necessary
     def receive_event(self, event):
         print("foo received")
         self.send_event(event, event.spread_up, event.spread_down)
 
-
-    #send information to the commanding node or child nodes
-    def send_event(self, event, send_parent = False, send_child = True):
-        if(send_parent and self.parent):
+    # send information to the commanding node or child nodes
+    def send_event(self, event, send_parent=False, send_child=True):
+        if send_parent and self.parent:
             print("parent bar sent")
-        if(send_child):
-            if (self.left):
-                print("left bar sent")
-            if (self.right):
-                print("right bar sent")
+        if send_child:
+            if self.children:
+                for child in self.children:
+                    print("Child event sent")
+
 
 class Root(Node):
-    #print tree in order from left to right
+    # print tree in order from left to right
     def inorder(self, node):
+        nodes = []
         if node is not None:
-            self.inorder(node.left)
-            print(node.key, end=' ')
-            self.inorder(node.right)
+            nodes.append(node.key)
+            for child in node.children:
+                childNodes = self.inorder(child)
+                nodes += childNodes
+        return nodes
 
-    #insert a node into the tree
-    def insert(self, key, parent_key):
+    def weightNode(self, node):
+        weight = 0
+        if node is not None:
+            weight += node.weight
+            for child in node.children:
+                childWeight = self.weightNode(child)
+                weight += childWeight
+        return weight
+
+    # insert a node into the tree
+    def insert(self, key, parent_key, weight):
         if self is None:
-            return Node(key, None)
+            return Node(key, None, weight)
 
         parent = self.find(self, parent_key)
-        if(parent.left == None):
-            parent.left = Node(key, parent)
-        elif(parent.right == None):
-            parent.right = Node(key, parent)
-        else:
-            print("Parent already has two child nodes")
+        parent.children.append(Node(key, parent, weight))
         return self
 
-    #find a node in the tree by it's key value
+    # find a node in the tree by its key value
     def find(self, node, x):
         if node.key == x:
             return node
-        if(node.left != None):
-            n = self.find(node.left, x)
-            if n:
-                return n
-        if(node.right != None):
-            n = self.find(node.right, x)
+        for child in node.children:
+            n = self.find(child, x)
             if n:
                 return n
         return None
 
-    #delete a node from a tree
+    # delete a node from a tree
     def delete_node(self, node):
         if node is None:
             return
 
-        if node.left:
-            replacement = node.left
-            replacement.parent = node.parent
-            if node.parent:
-                if node.parent.left == node:
-                    node.parent.left = replacement
-                else:
-                    node.parent.right = replacement
+        if node.parent:
+            node.parent.children.remove(node)
+            if node.parent.children:
+                childWeight = []
+                for child in node.parent.children:
+                    childWeight.append(self.weightNode(child))
+                newParent = node.parent.children[childWeight.index(min(childWeight))]
+                for child in node.children:
+                    child.parent = newParent
+                    newParent.children.append(child)
             else:
-                node = replacement
-
-            if replacement.right:
-                #if the replacing node has a right child, move that right child to the replacing node's left child
-                self.move_left_children_up(replacement.left, replacement.right)
-                replacement.right = None
-
-            #move the replaced node's right child to the replacing node
-            replacement.right = node.right
-            if node.right:
-                node.right.parent = replacement
+                for child in node.children:
+                    child.parent = node.parent
         else:
-            if node.parent:
-                if node.parent.left == node:
-                    node.parent.left = node.right
-                else:
-                    node.parent.right = node.right
-            else:
-                node = None
-
-    def move_left_children_up(self, left_child, right_subtree):
-        #if left child exist, attach the right child to it
-        if left_child:
-            right = left_child.right
-            left_child.right = right_subtree
-            right_subtree.parent = left_child
-            if(right != None):
-                self.move_left_children_up(left_child.left, right)
+            node = None
 
 
 if __name__ == '__main__':
-    #tree rebuilding testing
-    root = Root(5, None)
-    root.insert(2, 5)
-    root.insert(7, 2)
-    root.insert(6, 2)
-    root.insert(8, 7)
-    root.insert(9, 7)
-    root.insert(10, 8)
-    root.insert(11, 8)
-    root.insert(12, 10)
-    node = root.find(root, 2)
+    # tree rebuilding testing
+    root = Root(5, None, 5)
+    root.insert(2, 5, 1)
+    root.insert(7, 2, 1)
+    root.insert(6, 2, 1)
+    root.insert(8, 7, 1)
+    root.insert(9, 7, 2)
+    root.insert(10, 8, 6)
+    root.insert(11, 8, 3)
+    root.insert(12, 10, 2)
+    node = root.find(root, 8)
     root.delete_node(node)
-    root.inorder(root)
-
+    print(root.inorder(root))
